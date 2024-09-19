3ba380a79c1a66806e21140b9ad23df3a1dbd59e
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-09-19 19:40:22 +0530

    Spring security - csrf protection implemented

--- 12. Spring Security/school/src/main/java/com/darklord/school/config/ProjectSecurityConfig.java
+++ 12. Spring Security/school/src/main/java/com/darklord/school/config/ProjectSecurityConfig.java
@@ -10,16 +10,33 @@ public class ProjectSecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // THIS WILL PERMIT ALL APIS
+        /*
         http
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().permitAll()  // All requests are permitted without authentication
-                )
+                );
+         */
+
+        // This is custom security
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/","/home").permitAll()
+                        .requestMatchers("/holidays").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/courses").authenticated()
+                        .requestMatchers("/assets/**").permitAll()
+
+                ).formLogin(formLogin -> formLogin.permitAll());
+
         // THIS WILL DENY ALL APIS
-//        http
-//                .authorizeHttpRequests((requests) -> requests
-//                        .anyRequest().denyAll()  // All requests are permitted without authentication
-//                )
-//        ;
+        /*
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().denyAll()  // All requests are permitted without authentication
+                );
+         */
         return http.build();
     }
 }
